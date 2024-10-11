7fe1554324d9bfa2dd96e46071a674d50ac8a0d1
omarghandour <omarkhaled9913@gmail.com>
2024-10-11 18:08:21 +0300

    Skeleton effect

--- app/components/Arrivals.tsx
+++ app/components/Arrivals.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import Image from "next/image";
-import Watch from "../../public/watch.png";
 import { useEffect, useState } from "react";
-import { toast, useToast } from "@/hooks/use-toast";
-import Link from "next/link";
 import axios from "axios";
+import { useToast } from "@/hooks/use-toast";
+import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton"; // Import Skeleton from ShadCN UI
 
 type Product = {
   _id: string;
@@ -19,8 +18,10 @@ type Product = {
 
 const Arrivals = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true); // Loading state
   const { toast } = useToast();
   const [activeProductId, setActiveProductId] = useState<string | null>(null);
+
   useEffect(() => {
     // Fetch products from the backend
     const fetchProducts = async () => {
@@ -34,6 +35,8 @@ const Arrivals = () => {
         setProducts(response.data);
       } catch (error) {
         console.error("Failed to fetch products", error);
+      } finally {
+        setLoading(false); // Set loading to false when data is fetched
       }
     };
     fetchProducts();
@@ -44,19 +47,13 @@ const Arrivals = () => {
     setTimeout(() => {
       setActiveProductId(null); // Clear the animation after 1 second
     }, 1000); // Match the transition duration (1000ms)
-    // Set the active product id
-    // Get the current cart from localStorage
     const cart = JSON.parse(localStorage.getItem("cart") || "[]");
-    // Check if the product already exists in the cart
     const existingProduct = cart.find((item: any) => item._id === product._id);
     if (existingProduct) {
-      // If the product exists, increment its quantity
       existingProduct.quantity += 1;
     } else {
-      // Otherwise, add the product with an initial quantity of 1
       cart.push({ ...product, quantity: 1 });
     }
-    // Save the updated cart back to localStorage
     localStorage.setItem("cart", JSON.stringify(cart));
     toast({
       title: product.name,
@@ -64,68 +61,73 @@ const Arrivals = () => {
       action: <Link href="/cart">Go to cart</Link>,
     });
   };
-  products;
 
   return (
-    <div className="mb-14 text-white flex justify-center flex-col items-center w-full bg-[#FCFCFC] pt-10 p-2 text-center mx-auto xl:w-3/4">
+    <div
+      id="newarraival"
+      className="mb-14 text-white flex justify-center flex-col items-center w-full bg-[#FCFCFC] pt-10 p-2 text-center mx-auto xl:w-3/4"
+    >
       <div className="border-t-2 border-two w-20 p-1 font-medium"></div>
       <h2 className="text-[#2E2E2E] font-bold">NEW ARRIVALS</h2>
       <div className="grid gap-4 md:gap-16 xl:gap-20 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
-        {products.map((product, index) => (
-          <div
-            key={index}
-            className="mt-4 h-full justify-between md:mt-6 border-solid md:w-[225px]  border-2 border-[#F0F0F0] flex flex-col items-center md:px-4 xl:px-4 p-4 gap-2 relative shadow-xl transition-transform duration-300 ease-in-out transform md:hover:scale-105"
-          >
-            <Link
-              href={`/product/${product._id}`}
-              className="flex flex-col gap-4 justify-around h-full"
-            >
-              <span className="-rotate-90 bg-main py-2 px-2 absolute font-bold -left-[6px] md:top-2 top-[10px]">
-                SALE
-              </span>
-              <img
-                src={product.img}
-                // width={100}
-                loading="lazy"
-                alt={product.name}
-                className="w-full"
+        {loading
+          ? // Display skeletons when loading
+            Array.from({ length: 4 }).map((_, index) => (
+              <Skeleton
+                key={index}
+                className="h-[350px] w-full md:w-[225px] bg-gray-200 rounded-lg"
               />
-              {/* <img
-              src={Watch}
-              alt={product.name}
-              className="w-[140px] h-[215px]"
-            /> */}
-              <div>
-                <h1 className="text-[#2E2E2E] font-bold text-3xl">
-                  {product.name}
-                </h1>
-                <p className="text-[#595959] font-bold text-base line-through">
-                  {product.before} L.E
-                </p>
-                <p className="text-two font-bold text-2xl ">
-                  {product.price} L.E
-                </p>
-              </div>
-            </Link>
-            <button
-              onClick={() => addToCart(product)} // Pass the product's id
-              className="relative h-[10%] flex items-center justify-center whitespace-nowrap px-4 py-1 md:py-3 bg-main text-white font-semibold border overflow-hidden group"
-            >
+            ))
+          : products.map((product, index) => (
               <div
-                className={`absolute inset-0 md:group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform md:!duration-500 !duration-1000 ease-in-out center ${
-                  activeProductId === product._id
-                    ? "group-hover:translate-x-0"
-                    : ""
-                }`}
+                key={index}
+                className="mt-4 h-full justify-between md:mt-6 border-solid md:w-[225px]  border-2 border-[#F0F0F0] flex flex-col items-center md:px-4 xl:px-4 p-4 gap-2 relative shadow-xl transition-transform duration-300 ease-in-out transform md:hover:scale-105"
               >
-                ADD TO CART
+                <Link
+                  href={`/product/${product._id}`}
+                  className="flex flex-col gap-4 justify-around h-full"
+                >
+                  <span className="-rotate-90 bg-main py-2 px-2 absolute font-bold -left-[6px] md:top-2 top-[10px]">
+                    SALE
+                  </span>
+                  <img
+                    src={product.img}
+                    loading="lazy"
+                    alt={product.name}
+                    className="w-full"
+                  />
+                  <div>
+                    <h1 className="text-[#2E2E2E] font-bold text-3xl">
+                      {product.name}
+                    </h1>
+                    <p className="text-[#595959] font-bold text-base line-through">
+                      {product.before} L.E
+                    </p>
+                    <p className="text-two font-bold text-2xl ">
+                      {product.price} L.E
+                    </p>
+                  </div>
+                </Link>
+                <button
+                  onClick={() => addToCart(product)} // Pass the product's id
+                  className="relative h-[10%] flex items-center justify-center whitespace-nowrap px-4 py-1 md:py-3 bg-main text-white font-semibold border overflow-hidden group"
+                >
+                  <div
+                    className={`absolute inset-0 md:group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform md:!duration-500 !duration-1000 ease-in-out center ${
+                      activeProductId === product._id
+                        ? "group-hover:translate-x-0"
+                        : ""
+                    }`}
+                  >
+                    ADD TO CART
+                  </div>
+                  ADD TO CART
+                </button>
               </div>
-              ADD TO CART
-            </button>
-          </div>
-        ))}
+            ))}
       </div>
     </div>
   );
 };
+
 export default Arrivals;
--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useToast } from "@/hooks/use-toast";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton"; // Import Skeleton component
+
 type Product = {
   _id: string;
   name: string;
@@ -13,12 +15,14 @@ type Product = {
   countInStock: number;
   img: string;
 };
+
 const Featured = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true); // Manage loading state
   const { toast } = useToast();
   const [activeProductId, setActiveProductId] = useState<string | null>(null);
+
   useEffect(() => {
-    // Fetch products from the backend
     const fetchProducts = async () => {
       try {
         const response = await axios.get(
@@ -30,6 +34,8 @@ const Featured = () => {
         setProducts(response.data);
       } catch (error) {
         console.error("Failed to fetch products", error);
+      } finally {
+        setIsLoading(false); // Set loading to false once data is fetched
       }
     };
 
@@ -40,24 +46,17 @@ const Featured = () => {
     setActiveProductId(product._id);
     setTimeout(() => {
       setActiveProductId(null); // Clear the animation after 1 second
-    }, 1000); // Match the transition duration (1000ms)
+    }, 1000);
 
-    // Set the active product id
-    // Get the current cart from localStorage
     const cart = JSON.parse(localStorage.getItem("cart") || "[]");
-
-    // Check if the product already exists in the cart
     const existingProduct = cart.find((item: any) => item._id === product._id);
 
     if (existingProduct) {
-      // If the product exists, increment its quantity
       existingProduct.quantity += 1;
     } else {
-      // Otherwise, add the product with an initial quantity of 1
       cart.push({ ...product, quantity: 1 });
     }
 
-    // Save the updated cart back to localStorage
     localStorage.setItem("cart", JSON.stringify(cart));
 
     toast({
@@ -75,52 +74,65 @@ const Featured = () => {
     <div className="flex flex-col items-center w-full bg-[#FCFCFC] pt-10 p-2 mx-auto xl:w-3/4">
       <div className="border-t-2 border-two w-20 p-1 font-medium"></div>
       <h2 className="text-[#2E2E2E] font-bold text-xl md:text-2xl">FEATURED</h2>
-      <div className="grid gap-4 md:gap-8 xl:gap-12 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
-        {products.map((product, index) => (
-          <div
-            key={index}
-            className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center p-4 gap-2 shadow-lg transition-transform duration-300 transform md:hover:scale-105 w-full md:w-[225px]"
-          >
-            <Link
-              href={`/product/${product._id}`}
-              className="flex flex-col gap-2 justify-between h-full"
-            >
-              <span className="rotate-90 text-white bg-main py-1 px-2 absolute font-bold -left-[6px] top-4 text-xs md:text-sm">
-                SALE
-              </span>
-              <img
-                src={product.img}
-                loading="lazy"
-                alt={product.name}
-                className="w-full object-cover h-40 md:h-64"
-              />
-              <div className="text-center">
-                <h1 className="text-[#2E2E2E] font-bold text-lg md:text-xl truncate w-full">
-                  {product.name}
-                </h1>
-                <p className="text-[#595959] font-bold text-sm line-through">
-                  {product.before} L.E
-                </p>
-                <p className="text-two font-bold text-lg md:text-2xl">
-                  {product.price} L.E
-                </p>
-              </div>
-            </Link>
-            <button
-              onClick={() => addToCart(product)}
-              className="relative h-10 flex items-center justify-center px-4 py-2 bg-main text-white font-semibold border overflow-hidden group"
-            >
+      <div className="grid gap-4 md:gap-8 xl:gap-16 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
+        {isLoading
+          ? Array.from({ length: 4 }).map((_, index) => (
               <div
-                className={`absolute inset-0 group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform duration-500 ease-in-out ${
-                  activeProductId === product._id ? "translate-x-0" : ""
-                }`}
+                key={index}
+                className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center p-4 gap-2 shadow-lg w-full md:w-[225px]"
               >
-                <span className="truncate center">ADD TO CART</span>
+                <Skeleton className="w-full h-40 md:h-64" />
+                <Skeleton className="w-full h-6" />
+                <Skeleton className="w-1/2 h-5" />
+                <Skeleton className="w-1/3 h-7" />
+                <Skeleton className="w-full h-10" />
               </div>
-              <span className="truncate">ADD TO CART</span>
-            </button>
-          </div>
-        ))}
+            ))
+          : products.map((product, index) => (
+              <div
+                key={index}
+                className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center p-4 gap-2 shadow-lg transition-transform duration-300 transform md:hover:scale-105 w-full md:w-[225px]"
+              >
+                <Link
+                  href={`/product/${product._id}`}
+                  className="flex flex-col gap-2 justify-between h-full"
+                >
+                  <span className="rotate-90 text-white bg-main py-1 px-2 absolute font-bold -left-[6px] top-4 text-xs md:text-sm">
+                    SALE
+                  </span>
+                  <img
+                    src={product.img}
+                    loading="lazy"
+                    alt={product.name}
+                    className="w-full object-cover h-40 md:h-64"
+                  />
+                  <div className="text-center">
+                    <h1 className="text-[#2E2E2E] font-bold text-lg md:text-xl truncate w-full">
+                      {product.name}
+                    </h1>
+                    <p className="text-[#595959] font-bold text-sm line-through">
+                      {product.before} L.E
+                    </p>
+                    <p className="text-two font-bold text-lg md:text-2xl">
+                      {product.price} L.E
+                    </p>
+                  </div>
+                </Link>
+                <button
+                  onClick={() => addToCart(product)}
+                  className="relative h-10 flex items-center justify-center px-4 py-2 bg-main text-white font-semibold border overflow-hidden group"
+                >
+                  <div
+                    className={`absolute inset-0 group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform duration-500 ease-in-out ${
+                      activeProductId === product._id ? "translate-x-0" : ""
+                    }`}
+                  >
+                    <span className="truncate center">ADD TO CART</span>
+                  </div>
+                  <span className="truncate">ADD TO CART</span>
+                </button>
+              </div>
+            ))}
       </div>
     </div>
   );
