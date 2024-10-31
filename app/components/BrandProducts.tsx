cc1b4c757b28c8668a0013395a23b705635e47da
omarghandour <omarkhaled9913@gmail.com>
2024-10-31 21:02:55 +0300

    coming soon if the brand's product is zero

--- app/components/BrandProducts.tsx
+++ app/components/BrandProducts.tsx
@@ -35,6 +35,7 @@ const BrandProducts: React.FC = () => {
           }
         );
         setProducts(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("Failed to fetch products", error);
       } finally {
@@ -79,63 +80,69 @@ const BrandProducts: React.FC = () => {
       <h2 className="text-main font-bold text-xl md:text-2xl">
         {brand.toUpperCase()} PRODUCTS
       </h2>
-      <div className="grid gap-4 md:gap-8 xl:gap-16 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
-        {isLoading
-          ? Array.from({ length: 4 }).map((_, index) => (
-              <div
-                key={index}
-                className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center p-4 gap-2 shadow-lg w-full md:w-[225px]"
+      {isLoading ? (
+        <div className="grid gap-4 md:gap-8 xl:gap-16 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <div
+              key={index}
+              className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center p-4 gap-2 shadow-lg w-full md:w-[225px]"
+            >
+              <Skeleton className="w-full h-40 md:h-64" />
+              <Skeleton className="w-full h-6" />
+              <Skeleton className="w-1/2 h-5" />
+              <Skeleton className="w-1/3 h-7" />
+              <Skeleton className="w-full h-10" />
+            </div>
+          ))}
+        </div>
+      ) : products.length === 0 ? (
+        <p className="text-gray-500 mt-10 text-lg">Coming soon</p>
+      ) : (
+        <div className="grid gap-4 md:gap-8 xl:gap-16 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
+          {products.map((product) => (
+            <div
+              key={product._id}
+              className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center pb-8 gap-2 shadow-lg transition-transform duration-300 transform md:hover:scale-105 w-full md:w-[225px]"
+            >
+              <Link
+                href={`/product/${product._id}`}
+                className="flex w-full flex-col gap-2 justify-between h-full"
               >
-                <Skeleton className="w-full h-40 md:h-64" />
-                <Skeleton className="w-full h-6" />
-                <Skeleton className="w-1/2 h-5" />
-                <Skeleton className="w-1/3 h-7" />
-                <Skeleton className="w-full h-10" />
-              </div>
-            ))
-          : products.map((product) => (
-              <div
-                key={product._id}
-                className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center pb-8 gap-2 shadow-lg transition-transform duration-300 transform md:hover:scale-105 w-full md:w-[225px]"
+                <img
+                  src={product.img}
+                  loading="lazy"
+                  alt={product.name}
+                  className="w-full object-cover h-40 md:h-64"
+                />
+                <div className="text-center">
+                  <h1 className="text-main font-bold text-lg md:text-xl truncate w-full">
+                    {product.name}
+                  </h1>
+                  <p className="text-[#595959] text-sm line-through">
+                    {product.before} L.E
+                  </p>
+                  <p className="text-two font-bold text-xl md:text-2xl">
+                    {product.price} L.E
+                  </p>
+                </div>
+              </Link>
+              <button
+                onClick={() => addToCart(product)}
+                className="relative h-10 flex items-center justify-center px-4 py-2 bg-main text-white font-semibold border overflow-hidden group"
               >
-                <Link
-                  href={`/product/${product._id}`}
-                  className="flex w-full flex-col gap-2 justify-between h-full"
+                <div
+                  className={`absolute center inset-0 group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform duration-500 ease-in-out ${
+                    activeProductId === product._id ? "translate-x-0" : ""
+                  }`}
                 >
-                  <img
-                    src={product.img}
-                    loading="lazy"
-                    alt={product.name}
-                    className="w-full object-cover h-40 md:h-64"
-                  />
-                  <div className="text-center">
-                    <h1 className="text-main font-bold text-lg md:text-xl truncate w-full">
-                      {product.name}
-                    </h1>
-                    <p className="text-[#595959] text-sm line-through">
-                      {product.before} L.E
-                    </p>
-                    <p className="text-two font-bold text-xl md:text-2xl">
-                      {product.price} L.E
-                    </p>
-                  </div>
-                </Link>
-                <button
-                  onClick={() => addToCart(product)}
-                  className="relative h-10 flex items-center justify-center px-4 py-2 bg-main text-white font-semibold border overflow-hidden group"
-                >
-                  <div
-                    className={`absolute center inset-0 group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform duration-500 ease-in-out ${
-                      activeProductId === product._id ? "translate-x-0" : ""
-                    }`}
-                  >
-                    <span className="truncate center">ADD TO CART</span>
-                  </div>
-                  <span className="truncate">ADD TO CART</span>
-                </button>
-              </div>
-            ))}
-      </div>
+                  <span className="truncate center">ADD TO CART</span>
+                </div>
+                <span className="truncate">ADD TO CART</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
